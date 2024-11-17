99249138c33b98f71e6fd49faa9f56cac6172409
linco89 <sebastinesebastine2@gmail.com>
2024-11-17 17:54:26 +0100

    new file

---
+++ week 2 ass.py
@@ -0,0 +1,24 @@
+#question 1
+my_list = []
+
+#question 2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#question 3
+my_list[1]=15
+
+#question 4
+my_list.extend([50,60,70])
+
+#question 5
+my_list.remove(70)
+
+#question 6
+my_list.sort()
+
+#question 7
+index=my_list.index(30)
+print(my_list)
